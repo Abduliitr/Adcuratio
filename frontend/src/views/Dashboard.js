c582c6f2b541135e8d59ae1c31365c495cddbf41
Abduliitr <abdul.thomso@gmail.com>
2021-01-07 13:26:45 +0530

    Ready to submit

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // reactstrap components
 import { Card,CardHeader,CardBody,CardFooter,CardTitle,Row,Col} from "reactstrap";
 import { MDBDataTable } from 'mdbreact';
